4193979ab11d0324f80a6f15fc4aab233357b167
INTER-WEB-LATES\default <ggajo22@naver.com>
2016-12-29 19:08:34 +0900

    시간표 틀 추가

--- result.php
+++ result.php
@@ -2,6 +2,33 @@
 require("view/header.php");
 require_once("proc/dao_result.php");
 
+// SAT2 리스트 가져오기
+$sql = "SELECT test_id, test_sort, test_subject FROM test WHERE test_sort='SAT2' GROUP BY test_subject";
+$result = mysqli_query($conn, $sql);
+$sat2List = array();
+while($row = mysqli_fetch_assoc($result))
+{
+  $sat2List[] = $row;
+}
+
+// AP 리스트 가져오기
+$sql = "SELECT test_id, test_sort, test_subject FROM test WHERE test_sort='AP' GROUP BY test_subject";
+$result = mysqli_query($conn, $sql);
+$apList = array();
+while($row = mysqli_fetch_assoc($result))
+{
+  $apList[] = $row;
+}
+
+// test_info 가져오기
+$sql = "SELECT * FROM test_info as ti LEFT JOIN test as t ON ti.test_id = t.test_id";
+$result = mysqli_query($conn, $sql);
+$testInfo = array();
+while($row = mysqli_fetch_assoc($result))
+{
+  $testInfo[] = $row;
+}
+
 ?>
 
 
@@ -97,6 +124,24 @@ require_once("proc/dao_result.php");
 
 <input type="button" value="시간표 보러가기" class="btn btn-success btn-lg">
 
+<table id="timetable" class="table table-hover">
+  <thead>
+    <th>1주차</th>
+    <th>2주차</th>
+    <th>3주차</th>
+    <th>4주차</th>
+  </thead>
+  <tbody></tbody>
+  <tfoot></tfoot>
+</table>
+
+<ul id="check" class="list-group">
+  <?php
+    foreach($sat2List as $sl){
+      echo '<li id="test_'.$sl['test_id'].'" class="list-group-item" onclick="select_subject();">'.$sl['test_subject'].'</li>';
+    }
+  ?>
+</ul>
 
 <script>
   // 학생 정보 뿌리기
@@ -128,10 +173,12 @@ require_once("proc/dao_result.php");
   var uni = <?=json_encode($university);?>;
   var mus = $(".match_uni_skeleton");
 
+  // 학교와 학생 conv 비교
   var conv = 0;
   var dif = new Array();
   $.each(uni, function(index, score){
     conv = ((score.uni_sat*500/1600) + (score.uni_act*500/36))/2 + (score.uni_gpa_uw*100/4.0);
+    // 학교가 더 높아야함
     if(conv-studConv > 0){
       diff = {'idx':index, 'dix':conv-studConv};
       dif.push(diff);
@@ -140,17 +187,52 @@ require_once("proc/dao_result.php");
   dif.sort(function(a,b){
     return a.dix - b.dix;
   });
-  console.log(dif);
 
+  // 학생과 가장 비슷한 3개학교
   for(i=0; i<3; i++){
     var mashedObj = mashSkeleton(mus, uni[dif[i].idx], true);
     $('#match_uni').append(mashedObj);
   }
 
+  // 학생 상태보다 9~12 단계 높은 학교
   for(i=9; i<12; i++){
     var mashedObj = mashSkeleton(mus, uni[dif[i].idx], true);
     $('#dream_uni').append(mashedObj);
   }
 
-  $.post()
+// 시간표 작성
+
+  //테이블 만들기
+  var tbStr = '';
+  for(i=0; i<4; i++){
+    tbStr += '<tr>'
+    for(j=0; j<4; j++){
+      tbStr +='<td id="'+i+'_'+j+'">1</td>';
+    }
+    tbStr += '</tr>'
+  }
+  $('#timetable tbody').append(tbStr);
+
+  // 테이블에 각 과목들 hidden 으로 넣어놓기
+  var abc = '';
+  var subject = '';
+  var testInfo = <?=json_encode($testInfo);?>;
+  console.log(testInfo);
+  $.each(testInfo, function(index, info){
+    abc = '<div class="hidden" id="'+info.test_id+'_'+info.timeslot+'_'+info.weekslot+'">'+info.test_subject+'</div>';
+    $('#'+info.timeslot+'_'+info.weekslot).append(abc);
+  })
+
+  console.log(abc);
+
+  // 해당 버튼 눌렀을 때, 과목들 hidden 된 것 해지
+  function select_subject(){
+    $.each(testInfo, function(index, info){
+      $('#test_'+info.test_id).click(function(){
+        $('#'+info.test_id+'_'+info.timeslot+'_'+info.weekslot).toggleClass('hidden');
+        $('#test_'+info.test_id).toggleClass('active');
+      })
+    })
+  }
+
 </script>
